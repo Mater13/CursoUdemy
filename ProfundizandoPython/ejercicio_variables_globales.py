ae220cab534fce975d83d5540086bc30081a7808
MatiGit <matismedina23@gmail.com>
2023-11-07 17:50:09 -0300

    Profundizando python

---
+++ ProfundizandoPython/ejercicio_variables_globales.py
@@ -0,0 +1,16 @@
+# Definimos variable global
+contador = 0
+
+
+def mostrar_contador():
+    print(contador)
+
+
+def modificar_contador(c):
+    # Sin esta línea no se usa la variable global
+    global contador
+    contador = c
+
+
+modificar_contador(5)
+mostrar_contador()
